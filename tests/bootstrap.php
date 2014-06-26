b15c45bb37627de3ae0aca9799a6ecef9f20f4c1
Mpro <mpromain@gmail.com>
2014-06-26 07:36:18 +0200

    not using config.local.neon (very simply app)

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -17,5 +17,4 @@ $configurator->createRobotLoader()
 	->register();
 
 $configurator->addConfig(__DIR__ . '/../app/config/config.neon');
-$configurator->addConfig(__DIR__ . '/../app/config/config.local.neon');
 return $configurator->createContainer();
