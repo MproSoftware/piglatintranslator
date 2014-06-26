45a3dd5bc8377630763c864b707cb3a933696a7c
Mpro <mpromain@gmail.com>
2014-06-26 07:34:01 +0200

    Front implementation form for user service

--- app/presenters/HomepagePresenter.php
+++ app/presenters/HomepagePresenter.php
@@ -4,6 +4,8 @@ namespace PigLatinTranslator\Presenters;
 
 use Nette,
 	PigLatinTranslator\Model;
+use Nette\Application\UI\Form;
+use Kdyby\BootstrapFormRenderer\BootstrapRenderer;
 
 
 /**
@@ -12,9 +14,21 @@ use Nette,
 class HomepagePresenter extends BasePresenter
 {
 
-	public function renderDefault()
+	public function createComponentTranslatorForm()
 	{
-		$this->template->anyVariable = 'any value';
+		$form = new Form;
+		$form->setRenderer(new BootstrapRenderer);
+		$form->addText('text', 'Text:')
+			->setRequired('Zadejte prosím text.');
+		$form->addSubmit('translate', 'Přeložit');
+        $form->onSuccess[] = array($this, 'translatorFormSubmitted');
+        return $form;
+	}
+
+	public function translatorFormSubmitted($form)
+	{
+		$translator = $this->context->getByType('PigLatinTranslator\Services\ITranslator');
+		$this->template->translate = $translator->translate($form->values->text);
 	}
 
 }
